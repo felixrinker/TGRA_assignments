e6b9011ee2d9e5a046866900c1542e744c057bf9
Felix Rinker <Felix.Rinker@tuwien.ac.at>
2011-08-26 19:34:24 +0000

    Added new core to app starter class.

--- TGRA_assign1/src/main/java/com/tgra/ws11/desktop/DesktopAppStarter.java
+++ TGRA_assign1/src/main/java/com/tgra/ws11/desktop/DesktopAppStarter.java
@@ -2,6 +2,7 @@ package com.tgra.ws11.desktop;
 
 import com.badlogic.gdx.backends.jogl.JoglApplication;
 import com.tgra.ws11.core.TGRA_lab01_core;
+import com.tgra.ws11.core.TGRA_part1_core;
 
 public class DesktopAppStarter {
 
@@ -11,6 +12,9 @@ public class DesktopAppStarter {
 	public static void main(String[] args) {
 		
 		// lab01 core
-		new JoglApplication(new TGRA_lab01_core(), "TGRA - lab01", 1024, 768, false);
+		//new JoglApplication(new TGRA_lab01_core(), "TGRA - lab01", 1024, 768, false);
+		
+		// part1 core
+		new JoglApplication(new TGRA_part1_core(), "TGRA - part1", 1024, 768, false);
 	}
 }
