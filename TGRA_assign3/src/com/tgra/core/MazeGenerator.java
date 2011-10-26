76753e3f134fa6b131e9c7606e376c8cff3aee65
Felix Rinker <Felix.Rinker@gmail.com>
2011-10-26 20:27:31 +0000

    Complete maze generator

--- TGRA_assign3/src/com/tgra/core/MazeGenerator.java
+++ TGRA_assign3/src/com/tgra/core/MazeGenerator.java
@@ -5,15 +5,12 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Collections;
 import java.util.Arrays;
+import java.util.Stack;
 
 import com.tgra.model.Cell;
  
 /*
- * recursive backtracking algorithm
- * shamelessly borrowed from the ruby at
- * http://weblog.jamisbuck.org/2010/12/27/maze-generation-recursive-backtracking
- * Recursive backtracker
- * 
+
 The depth-first search algorithm of maze generation is frequently implemented using backtracking:
 Start with all walls in the maze intact
 
@@ -52,69 +49,107 @@ public class MazeGenerator {
 	public void generatePath() {
 		
 		ArrayList<Cell> visitN = new ArrayList<Cell>();
+		ArrayList<Cell> unvisitN = new ArrayList<Cell>();
 		
-		for (int x = 0; x < mazeHeight; x++) {
-			
+		Stack<Cell> stack = new Stack<Cell>();
+		for (int x = 0; x < mazeHeight-1; x++) {
 			for (int y = 1; y < mazeLength; y++) {
-				
-				if(this.cells[x][y].isSouthWall()) {
-					//System.out.print("+---");
-					
-					
-					ArrayList<Cell> foundN	= findNeighbours(x, y);
-					//System.out.println("found N: "+foundN);
-					
-					// sort out visited neighbors
-					ArrayList<Cell> unknownN = new ArrayList<Cell>();
-					for(Cell n : foundN) {
-						if(!visitN.contains(n)) unknownN.add(n);
-					}
-					
-					// choose one random from remaining
-					int index = randomGenerator.nextInt(unknownN.size());
-					Cell randomN = unknownN.get(index);		
-							
-					System.out.println("Random N: "+randomN.getPosX()+" Y: "+randomN.getPosY());
-					
-					if(randomN.getPosX() != x) {	
-						if(randomN.getPosY() > y) this.cells[x][y].setWestWall(false);
-						if(randomN.getPosY() < y) this.cells[x][randomN.getPosY()].setWestWall(false);
-					}
-					
-					if(randomN.getPosY() != y) {
-						if(randomN.getPosX() > x) this.cells[randomN.getPosX()][y].setSouthWall(false);
-						if(randomN.getPosX() < x) this.cells[x][y].setSouthWall(false);
-					}
-				}
+				unvisitN.add(cells[x][y]);
 			}
-			System.out.println("+");
-			
-			if(x < (mazeHeight-1)) {
-				for (int j = 0; j < this.mazeLength; j++) {
-			
-				
-				if(this.cells[x][j].isWestWall()) {
-					System.out.print("|   ");
-				}
-				
-			}
-			}
-			System.out.println(" ");
 		}
 		
+		Cell currentCell = cells[0][1];
+		// make currentCell visit
+		unvisitN.remove(currentCell);
+		visitN.add(currentCell);
+		stack.add(currentCell);
+		currentCell.setSouthWall(false);
+		while(!unvisitN.isEmpty()) {
+			
+			int curX = currentCell.getPosX();
+			int curY = currentCell.getPosY();
+			
+			ArrayList<Cell> foundN	= findNeighbours(curX, curY);
+			//System.out.println("found N: "+foundN);
+			
+			// sort out visited neighbors
+			ArrayList<Cell> unknownN = new ArrayList<Cell>();
+			for(Cell n : foundN) {
+				if(!visitN.contains(n)) unknownN.add(n);
+			}
+			if(!unknownN.isEmpty()) {	
+				// choose one random from remaining
+				int index = randomGenerator.nextInt(unknownN.size());
+				Cell randomN = unknownN.get(index);		
+						
+				//System.out.println("Random N: "+randomN.getPosX()+" Y: "+randomN.getPosY());
+				
+				
+				modifyWall(currentCell, randomN);
+				
+				// push randomN to visit stack and remove from unvisit
+				visitN.add(randomN);
+				unvisitN.remove(randomN);
+				
+				stack.add(randomN);
+				
+				// make random new current cell
+				currentCell = randomN;
+				
+			}else {
+				//if(unvisitN.isEmpty()) System.out.println("X: "+currentCell.getPosX()+" Y: "+currentCell.getPosY());
+				//if(!unvisitN.isEmpty()) currentCell = unvisitN.get(0);
+				
+				currentCell = stack.pop();
+			}	
+			//this.display();
+		}
+		
+		
+		this.display();
+		System.out.println("X: "+currentCell.getPosX()+" Y: "+currentCell.getPosY());
+		/*for(Cell c: visitN) {
+			System.out.println("X: "+c.getPosX()+" Y: "+c.getPosY());
+		}*/
+	}
+	
+	private void createExit(Cell cell) {
+		
+		int x = cell.getPosX();
+		int y = cell.getPosY();
+		
+		
+	}
+	
+	private void modifyWall(Cell currentCell, Cell neighCell) {
+		
+		int curX	= currentCell.getPosX();
+		int curY	= currentCell.getPosY();
+		int neX		= neighCell.getPosX();
+		int neY		= neighCell.getPosY();
+		
+		if(neX == curX) {	
+			if(neY > curY) this.cells[curX][curY].setWestWall(false);
+			if(neY < curY) this.cells[curX][neY].setWestWall(false);
+		}
+		
+		if(neY == curY) {
+			if(neX > curX) this.cells[neX][curY].setSouthWall(false);
+			if(neX < curX) this.cells[curX][curY].setSouthWall(false);
+		}
 	}
 	
 	private ArrayList<Cell> findNeighbours(int x, int y) {
 		
 		ArrayList<Cell> neighCells = new ArrayList<Cell>();
 		
-		if(x >= 0 && x< this.mazeHeight && y >= 1 && y< this.mazeLength) {
+		if(x >= 0 && x < this.mazeHeight-1 && y >= 1 && y< this.mazeLength) {
 			
 			if(y > 1) neighCells.add(cells[x][y-1]);
 			if(y < this.mazeLength-1) neighCells.add(cells[x][y+1]);
 			
 			if(x > 0) neighCells.add(cells[x-1][y]);
-			if(x < this.mazeHeight-1) neighCells.add(cells[x+1][y]);
+			if(x < this.mazeHeight-2) neighCells.add(cells[x+1][y]);
 		}
 		
 		return neighCells;
@@ -130,6 +165,7 @@ public class MazeGenerator {
 			}
 		}
 		
+		//this.cells[0][1].setSouthWall(false);
 	}
  
 	public void display() {
@@ -139,6 +175,8 @@ public class MazeGenerator {
 				
 				if(this.cells[i][j].isSouthWall()) {
 					System.out.print("---+");
+				}else{
+					System.out.print("   +");
 				}
 			}
 			System.out.println(" ");
@@ -147,6 +185,8 @@ public class MazeGenerator {
 				
 				if(this.cells[i][j].isWestWall()) {
 					System.out.print("   |");
+				}else {
+					System.out.print("    ");
 				}
 				
 			}
@@ -155,17 +195,9 @@ public class MazeGenerator {
 		}
 	}
  
-	
- 
-	private static boolean between(int v, int upper) {
-		return (v >= 0) && (v < upper);
-	}
- 
-	
- 
 	public static void main(String[] args) {
 		
-		MazeGenerator maze = new MazeGenerator(5, 5);
+		MazeGenerator maze = new MazeGenerator(20, 30);
 		maze.display();
 		System.out.println("aa");
 		
