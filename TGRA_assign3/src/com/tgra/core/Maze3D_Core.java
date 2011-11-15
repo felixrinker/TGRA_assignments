c6d0e4f74564a301325e7d6a96dde9d4e89dd584
Felix Rinker <Felix.Rinker@gmail.com>
2011-11-15 21:18:05 +0000

    Changed the perspective values. Also the light and material spec

--- TGRA_assign3/src/com/tgra/core/Maze3D_Core.java
+++ TGRA_assign3/src/com/tgra/core/Maze3D_Core.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL11;
 import com.tgra.camera.MazeCam;
-import com.tgra.model.Cube;
 import com.tgra.model.Maze;
 import com.tgra.model.Point3D;
 import com.tgra.model.Vector3D;
@@ -13,7 +12,6 @@ import com.tgra.model.Vector3D;
 public class Maze3D_Core implements ApplicationListener
 {
 	private MazeCam cam;
-	private Cube cube;
 	private Maze maze;
 	
 	@Override
@@ -23,7 +21,7 @@ public class Maze3D_Core implements ApplicationListener
 		Gdx.gl11.glEnable(GL11.GL_LIGHT0);
 		Gdx.gl11.glEnable(GL11.GL_LIGHT1);
 		Gdx.gl11.glEnable(GL11.GL_DEPTH_TEST);
-		
+		Gdx.gl11.glEnable(GL11.GL_NORMALIZE);
 		Gdx.gl11.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
 		
@@ -38,7 +36,6 @@ public class Maze3D_Core implements ApplicationListener
 		cam = new MazeCam(new Point3D(1f, 0.5f, 0.0f), new Point3D(0.0f, 0.5f, 0.5f), new Vector3D(0.0f, 1.0f, 0.0f));
 		maze.setCam(cam);
 		Gdx.gl11.glEnableClientState(GL11.GL_VERTEX_ARRAY);
-		cube = new Cube();
 	}
 	
 	
@@ -60,14 +57,14 @@ public class Maze3D_Core implements ApplicationListener
 	{
 		
 		float deltaTime = Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.UP))
+		/*if(Gdx.input.isKeyPressed(Input.Keys.UP))
 		{
 			cam.pitch(-90.0f * deltaTime);
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.DOWN))
 		{
 			cam.pitch(90.0f * deltaTime);
-		}
+		}*/
 
 		if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
 		{
@@ -119,51 +116,26 @@ public class Maze3D_Core implements ApplicationListener
 	{
 		
 		Gdx.gl11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT);
-
-		Gdx.gl11.glMatrixMode(GL11.GL_MODELVIEW);
-		Gdx.gl11.glLoadIdentity();
 		
+		// light 0
 		cam.setModelViewMatrix();
-		
-		float[] globalAmbience = {0.2f, 0.2f, 0.2f, 1.0f};
-		Gdx.gl11.glLightModelfv(GL11.GL_LIGHT_MODEL_AMBIENT, globalAmbience, 0);
-		
-		
-		float[] lightDiffuse =  {1.0f, 1.0f, 1.0f, 1.0f};
+		float[] lightDiffuse = {1.0f, 1.0f, 1.0f, 1.0f};
 		Gdx.gl11.glLightfv(GL11.GL_LIGHT0, GL11.GL_DIFFUSE, lightDiffuse, 0);
 
-		float[] lightSpecular =  {1.0f, 1.0f, 1.0f, 1.0f};
-		Gdx.gl11.glLightfv(GL11.GL_LIGHT0, GL11.GL_SPECULAR, lightSpecular, 0);
-
-		float[] lightPosition = {5.0f, 10.0f, 15.0f, 1.0f};
+		float[] lightPosition = {-15.0f, 8.0f, 5.0f, 1.0f};
 		Gdx.gl11.glLightfv(GL11.GL_LIGHT0, GL11.GL_POSITION, lightPosition, 0);
 
 		
-		
-		float[] lightDiffuse1 = {1.0f, 1.0f, 1.0f, 1.0f};
+		// light 1
+		float[] lightDiffuse1 = {0.5f, 0.5f, 0.5f, 1.0f};
 		Gdx.gl11.glLightfv(GL11.GL_LIGHT1, GL11.GL_DIFFUSE, lightDiffuse1, 0);
 
-		float[] lightSpecular1 = {1.0f, 1.0f, 1.0f, 1.0f};
-		Gdx.gl11.glLightfv(GL11.GL_LIGHT1, GL11.GL_SPECULAR, lightSpecular1, 0);
-		
-		float[] lightPosition1 = {-5.0f, -10.0f, -15.0f, 1.0f};
+		float[] lightPosition1 = {5.0f, -10.0f, -15.0f, 1.0f};
 		Gdx.gl11.glLightfv(GL11.GL_LIGHT1, GL11.GL_POSITION, lightPosition1, 0);
 
-		
-		
-		float[] materialDiffuse = {0.2f, 0.2f, 0.1f, 0.5f};
+		// material
+		float[] materialDiffuse = {1.0f, 1.0f, 1.0f, 1.0f};
 		Gdx.gl11.glMaterialfv(GL11.GL_FRONT, GL11.GL_DIFFUSE, materialDiffuse, 0);
-
-		float[] materialSpecular = {1.0f, 1.0f, 1.0f, 1.0f};
-		Gdx.gl11.glMaterialfv(GL11.GL_FRONT, GL11.GL_SPECULAR, materialSpecular, 0);
-
-		float[] materialAmbient = {0.2f, 0.6f, 0.1f, 1.0f};
-		Gdx.gl11.glMaterialfv(GL11.GL_FRONT, GL11.GL_AMBIENT, materialAmbient, 0);
-		
-		Gdx.gl11.glPushMatrix();
-		Gdx.gl11.glTranslatef(-1.0f, 5.0f, -1.0f);
-		cube.draw();
-		Gdx.gl11.glPopMatrix();
 		
 		this.maze.draw();	
 	}
@@ -185,7 +157,7 @@ public class Maze3D_Core implements ApplicationListener
 		Gdx.gl11.glMatrixMode (GL11.GL_PROJECTION);
 		Gdx.gl11.glLoadIdentity ();
 		
-		Gdx.glu.gluPerspective (Gdx.gl11, 45, (float) w / (float) h, 0.02f, 1000.0f );
+		Gdx.glu.gluPerspective (Gdx.gl11, 90, (float) w / (float) h, 0.02f, 100.0f );
 		Gdx.gl11.glMatrixMode (GL11.GL_MODELVIEW); //set the matrix back to model
 
 	}
