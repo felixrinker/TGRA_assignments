c7569b3962ed7326004d422965e0f6495bb3f486
Felix Rinker <Felix.Rinker@gmail.com>
2011-11-15 21:08:02 +0000

    Changed resolution to 1024

--- TGRA_assign3/src/com/tgra/starter/DesktopStarter.java
+++ TGRA_assign3/src/com/tgra/starter/DesktopStarter.java
@@ -7,7 +7,7 @@ public class DesktopStarter
 {
 	public static void main(String[] args)
 	{
-	//	new LwjglApplication(new First3D_Core(), "First 3D Program", 1024, 768, false);
-		new LwjglApplication(new Maze3D_Core(), "First 3D Program", 640, 480, false);
+	new LwjglApplication(new Maze3D_Core(), "First 3D Program", 1024, 768, false);
+		//new LwjglApplication(new Maze3D_Core(), "First 3D Program", 640, 480, false);
 	}
 }
